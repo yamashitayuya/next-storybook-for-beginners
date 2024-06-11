4c577690b67dd0af603ff188b81d6a30f2fac8ae
Naoya Yamamoto <naoya.yamamoto@fox-hound.jp>
2024-06-11 14:58:41 +0900

    fix: fetchをno-cacheで使用することでビルド時のエラーを解消 (#12)
    feat: pnpm をv9にアップデート

--- lib/api/index.ts
+++ lib/api/index.ts
@@ -2,6 +2,8 @@
  * APIによるデータの取得
  */
 export async function api<T>(path: string): Promise<T> {
-  const result = await fetch(`http://localhost:4010/v1/${path}`);
+  const result = await fetch(`http://localhost:4010/v1/${path}`, {
+    cache: 'no-store',
+  });
   return (await result.json()) as T;
 }
